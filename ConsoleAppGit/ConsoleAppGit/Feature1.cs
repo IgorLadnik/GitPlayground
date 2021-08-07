27d4f35d6307dee666a566c057e5253275b46ac8
IgorLadnik <ladnik@hotmail.com>
2021-08-07 09:30:18 +0300

    Feature1 added

---
+++ ConsoleAppGit/ConsoleAppGit/Feature1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGit
+{
+    public class Feature1
+    {
+        private string _data;
+
+        public Feature1(string data)
+        {
+            _data = data;
+        }
+    }
+}
